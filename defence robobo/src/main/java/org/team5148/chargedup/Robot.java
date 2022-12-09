62bdc10bddb444d32cabf487cae2df018adb7fa4
HexBewitched <theamazingmike111@gmail.com>
2022-12-08 19:23:07 -0600

    original push

---
+++ defence robobo/src/main/java/org/team5148/chargedup/Robot.java
@@ -0,0 +1,65 @@
+
+package org.team5148.chargedup;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  CANSparkMax drive = new CANSparkMax
+
+  @Override
+  public void robotInit() {
+    
+  }
+
+  @Override
+  public void robotPeriodic() {
+
+  }
+
+  @Override
+  public void autonomousInit() {
+    
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+
+    }
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+  }
+
+  @Override
+  public void disabledInit() {
+
+  }
+
+  @Override
+  public void disabledPeriodic() {
+
+  }
+
+  @Override
+  public void testInit() {
+
+  }
+
+  @Override
+  public void testPeriodic() {
+
+  }
+}
