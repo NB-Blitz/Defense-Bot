f50ef5e28354b1899182e13126e7504978a62282
leonardoGS-5148 <ajl13900@gmail.com>
2022-12-13 20:46:04 -0600

    Added new Code

--- defence robobo/src/main/java/org/team5148/chargedup/Robot.java
+++ defence robobo/src/main/java/org/team5148/chargedup/Robot.java
@@ -4,13 +4,18 @@ package org.team5148.chargedup;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.XboxController;
 
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-  CANSparkMax drive = new CANSparkMax
+  LeftOne drive = new CANSparkMax();
+  LeftTwo drive = new CANSparkMax();
+  RightOne drive = new CANSparkMax();
+  RightTwo drive = new CANSparkMax();
+  XboxController driveController = new XboxController(0);
 
   @Override
   public void robotInit() {
@@ -35,12 +40,16 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-
+    frontRight.setInverted(true);
+		backRight.setInverted(true);
   }
 
   @Override
   public void teleopPeriodic() {
-
+    Double ySpeed = driveController.getLeftY();
+		Double xSpeed = driveController.getLeftX();
+		Double zRotation = driveController.getRightX(); 
+		MDrive.driveCartesian(ySpeed, xSpeed, zRotation);
   }
 
   @Override
