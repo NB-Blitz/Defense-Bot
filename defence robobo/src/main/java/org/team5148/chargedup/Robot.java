ca70f64b209dd242d0a0a3ec4a0f89412154f54a
leonardoGS-5148 <ajl13900@gmail.com>
2023-01-05 20:39:35 -0600

    Update Robot.java

--- defence robobo/src/main/java/org/team5148/chargedup/Robot.java
+++ defence robobo/src/main/java/org/team5148/chargedup/Robot.java
@@ -1,22 +1,23 @@
 package org.team5148.chargedup;
 
-import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkMax;
 //import com.revrobotics.SparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.XboxController;
 
+
 //import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
+//import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-	CANSparkMax frontLeft = new CANSparkMax(1, MotorType.kBrushless);
-	CANSparkMax frontRight = new CANSparkMax(3, MotorType.kBrushless);
-	CANSparkMax backLeft = new CANSparkMax(2, MotorType.kBrushless);
-	CANSparkMax backRight = new CANSparkMax(4, MotorType.kBrushless);
+	//CANSparkMax frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+	//CANSparkMax frontRight = new CANSparkMax(3, MotorType.kBrushless);
+	//CANSparkMax backLeft = new CANSparkMax(2, MotorType.kBrushless);
+	//CANSparkMax backRight = new CANSparkMax(4, MotorType.kBrushless);
 	XboxController driveController = new XboxController(0);
-	MecanumDrive MDrive = new MecanumDrive(frontLeft , backLeft , frontRight , backRight);
+	//MecanumDrive MDrive = new MecanumDrive(frontLeft , backLeft , frontRight , backRight);
 
   @Override
   public void robotInit() {
@@ -49,7 +50,7 @@ public class Robot extends TimedRobot {
     Double ySpeed = driveController.getLeftY();
 		Double xSpeed = driveController.getLeftX();
 		Double zRotation = driveController.getRightX(); 
-		MDrive.driveCartesian(ySpeed, xSpeed, zRotation);
+		//MDrive.driveCartesian(ySpeed, xSpeed, zRotation);
   }
 
   @Override
